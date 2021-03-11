9a2a09a2c762f48c5c63fddcf55890eec73a4485
堀田達也 <hottatatsuya@hottatatsuyanoMacBook-Pro.local>
2021-03-11 21:52:40 +0900

    async await issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:soundpool/soundpool.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+import 'dart:math' as math;
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(Wrapper());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return Body();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+class Body extends StatefulWidget {
+  _Body createState() => _Body();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _Body extends State<Body> {
+  math.Random random = new math.Random();
+  Soundpool pool = Soundpool(streamType: StreamType.notification);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  int myHp = 10;
+  int eneHp = 8;
+  String myMsg = "敵が現れた";
+  bool isMyTurn = true;
+  bool warIsOver = false;
+  bool whenWarIsStart = true;
+
+  void myTurn() async {
+    int soundId =
+        await rootBundle.load("assets/sounds/beep.wav").then((soundData) {
+      return pool.load(soundData);
     });
+    await pool.play(soundId, rate: 1);
+    setState(() {
+      eneHp -= 1;
+      isMyTurn = false;
+      myMsg = "攻撃！";
+    });
+
+    if (eneHp < 1) {
+      setState(() {
+        myMsg = "勝利！";
+        warIsOver = true;
+      });
+    }
+  }
+
+  void eneTurn() async {
+    int soundId =
+        await rootBundle.load("assets/sounds/beep.wav").then((soundData) {
+      return pool.load(soundData);
+    });
+    await pool.play(soundId, rate: 0.25);
+    setState(() {
+      myHp -= 1;
+      myMsg = "被弾！";
+      isMyTurn = true;
+    });
+
+    print(myHp);
+
+    if (myHp < 1) {
+      setState(() {
+        myMsg = "敗北…";
+        warIsOver = true;
+      });
+    }
+  }
+
+  void turn() {
+    if (warIsOver) {
+      return;
+    }
+    if (whenWarIsStart) {
+      setState(() {
+        whenWarIsStart = false;
+        myHp += random.nextInt(5);
+        eneHp += random.nextInt(5);
+      });
+    }
+
+    if (isMyTurn) {
+      myTurn();
+    } else {
+      eneTurn();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    // TODO: implement build
+
+    return MaterialApp(
+        home: Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Welcome to Flutter'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: Text(myMsg),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        child: Icon(Icons.arrow_upward),
+        onPressed: turn,
+      ),
+    ));
+
+    throw UnimplementedError();
   }
 }
